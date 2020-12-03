00ff874a150764b6ecf9f640d4d924ca40b16ccb
Ion Leahu <forza11879@gmail.com>
2020-12-03 18:57:12 -0500

    tetting up routes, controlles and io connection

--- app.js
+++ app.js
@@ -1,21 +1,51 @@
 import express from 'express';
+import { createServer } from 'http';
+import socketio from 'socket.io';
+import path from 'path';
 import dotnev from 'dotenv';
 import cors from 'cors';
+import { stream, sendMessage } from './uitls/twitter.js';
 
 // routes
-import mainRoute from './routes/main.js';
-
-const app = express();
-
-app.use(cors());
+import mainRoute from './routes/tweets.js';
 
 dotnev.config({ path: './config/dev.env' });
 
+const app = express();
+const server = createServer(app);
+const io = socketio(server);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.locals.searchTerm = 'JavaScript'; //Default search term for twitter stream.
+app.locals.showRetweets = false; //Default
+
 // Mount Rout
-app.use('/', mainRoute);
+app.use('/', mainRoute(app, io));
+// require('./routes/tweets.js')(app, io);
 
 const port = process.env.PORT;
-const server = createServer(app);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 server.listen(port, function () {
   console.log(`Server is up on port ${port}`);
 });
+
+let socketConnection;
+let twitterStream;
+
+//Establishes socket connection.
+io.on('connection', (socket) => {
+  socketConnection = socket;
+  stream(app.locals.searchTerm, twitterStream);
+  socket.on('connection', () => console.log('Client connected'));
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+/**
+ * Emits data from stream.
+ * @param {String} msg
+ */
+
+sendMessage(msg, socketConnection);
